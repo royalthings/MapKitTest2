197e7f87cb5a2e7ebb1065ebbd989fc2bf316beb
Дмитрий Ага <royalthings2019@gmail.com>
2019-07-27 11:02:48 +0300

    Initial Commit

---
+++ MapKit_Test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKit_Test2
+//
+//  Created by Дмитрий Ага on 7/27/19.
+//  Copyright © 2019 Дмитрий Ага. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
