b7a190ccd07dd7347a880d224c752e7a66b26a9f
Дмитрий Ага <royalthings2019@gmail.com>
2019-07-27 18:40:11 +0300

    mapKit

---
+++ MapKit_Test2/MapViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MapViewController.swift
+//  MapKit_Test2
+//
+//  Created by Дмитрий Ага on 7/27/19.
+//  Copyright © 2019 Дмитрий Ага. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var closeBtn: UIButton!
+    var place: Place?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        closeBtn.layer.cornerRadius = 4
+        
+        
+        if let selectPlace = place {
+            let regionRadius: CLLocationDistance = 1000.0
+            let coordinateRegion = MKCoordinateRegion(center: selectPlace.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+            mapView.setRegion(coordinateRegion, animated: true)
+            mapView.addAnnotation(selectPlace)
+        }
+    }
+
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+    @IBAction func changeMapType(_ sender: UISegmentedControl) {
+
+        if sender.selectedSegmentIndex == 0 {
+            mapView.mapType = .standard
+        } else {
+            mapView.mapType = .satellite
+        }
+        
+    }
+    
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapViewWillStartLoadingMap(_ mapView: MKMapView) {
+        print("localiz")
+    }
+}
--- MapKit_Test2/ViewController.swift
+++ MapKit_Test2/ViewController.swift
@@ -7,14 +7,108 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Outlets
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var pikerView: UIPickerView!
+    
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var discLable: UILabel!
+    
+    var locationManager: CLLocationManager?
+    var currentLocation: CLLocation?
+    var places: [Place] = []
+    var selectedPlace: Place? = nil
+    
+    let images = [UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3")]
+
+    
+    
+//    let images = [UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"), UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"), UIImage(named: "10"), UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"), UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"), UIImage(named: "10"), UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"), UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"), UIImage(named: "10"), UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"), UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"), UIImage(named: "10"), UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"), UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"), UIImage(named: "10"), UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"), UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"), UIImage(named: "10"), UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"), UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"), UIImage(named: "10"), UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"), UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"), UIImage(named: "10"), UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"), UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"), UIImage(named: "10"), UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6"), UIImage(named: "7"), UIImage(named: "8"), UIImage(named: "9"), UIImage(named: "10")]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let rotationAngle: CGFloat = -90 * (.pi/180)
+        let y = pikerView.frame.origin.y
+        pikerView.transform = CGAffineTransform(rotationAngle: rotationAngle)
+        pikerView.frame = CGRect(x: -100, y: y, width: view.frame.width + 200, height: 50)
+        myImageView.image = images[0]
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        selectedPlace = places.first
+   
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mapSegue" {
 
+            guard let mapController = segue.destination as? MapViewController,
+                let selectedPlace = selectedPlace else { return }
+            mapController.place = selectedPlace
+        }
+    }
+
+    //MARK - Actions
+    @IBAction func pushBtnWasPresset(_ sender: Any) {
+        
+        let position = Int(arc4random_uniform(3))
+        
+        pikerView.selectRow(position, inComponent: 0, animated: true)
+        let actualRow = pikerView.selectedRow(inComponent: 0)
+        print("Вы выбрали элемент \(actualRow)")
+        
+        let places = Place.getPlaces()
+        
+        let selectPlace = places[actualRow]
+        titleLable.text = selectPlace.title
+        discLable.text = selectPlace.subtitle
+        let selectImage = UIImage(named: selectPlace.image!)
+        myImageView.image = selectImage
+        selectedPlace = selectPlace
+    }
+    
 }
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    //MARK: - UIPickerViewDataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return images.count
+    }
+    
+    //MARK: - UIPickerViewDelegate
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let vieww = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        
+        let myImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        myImage.image = images[row]
+        vieww.addSubview(myImage)
+        vieww.transform = CGAffineTransform(rotationAngle: (90 * (.pi/180)))
+        return vieww
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        myImageView.image = images[row]
+        let actualRow = pikerView.selectedRow(inComponent: 0)
+        print("Вы выбрали элемент \(actualRow)")
+    }
+    
+}
+extension ViewController: CLLocationManagerDelegate { }
 
